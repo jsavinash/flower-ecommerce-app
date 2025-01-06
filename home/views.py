949d13df82006c070d53a54ca1ad22182a930118
root <root@LIN-JHDSBK3.corp.capgemini.com>
2025-01-06 20:27:04 +0000

    code added

--- home/views.py
+++ home/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    context = {
+        'firstname': 'Avinash',
+        'lastname': 'Nishad'
+    }
+    return render(request, 'home/index.html', context)
